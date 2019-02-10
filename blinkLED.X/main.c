0a126c799603464d424ac19bae0898d8a035071c
Noelle Crane <Noelle Crane@10.186.161.217>
2019-02-10 15:05:35 -0500

    new

---
+++ blinkLED.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Noelle Crane
+ *
+ * Created on February 10, 2019, 2:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ blinkLED.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: Noelle Crane
+ *
+ * Created on February 10, 2019, 2:55 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
