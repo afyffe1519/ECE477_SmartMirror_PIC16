9194c7d44c2f7f3cdfa984d9acf1540ffa24bdd4
afyffe1519 <afyffe@purdue.edu>
2019-02-11 10:37:36 -0500

    added PWM file

---
+++ PWM.X/build/default/production/PWM.p1.d
@@ -0,0 +1,6 @@
+build/default/production/PWM.p1:  \
+PWM.c  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/adc.h  \
+mcc_generated_files/pwm1.h  \
+mcc_generated_files/tmr2.h 
---
+++ PWM.X/main.c
@@ -0,0 +1,122 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65
+        Device            :  PIC16F18345
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+#include "mcc_generated_files/mcc.h"
+/**
+  Section: Macro Declarations
+ */
+    #define _XTAL_FREQ          500000
+    #define PRESSED             1
+    #define NOT_PRESSED         0
+    #define RUNNING             1
+    #define NOT_RUNNING         0
+    #define LAST                16
+    #define LEDs_SetLow()       do { LATA = 0; LATCbits.LATC5 = 0; } while(0)
+
+    #define INTERRUPT_TMR0InterruptEnable()         do { TMR0IE = 1; } while(0)
+    #define INTERRUPT_TMR0InterruptDisable()        do { TMR0IE = 0; } while(0)
+
+/**
+  Section: Variable Definitions
+ */
+    uint8_t labNumber = 6;
+    uint8_t switchEvent = 0;
+    uint8_t labState = NOT_RUNNING;
+    uint8_t btnState = NOT_PRESSED;    
+    
+void checkButtonS1(void);
+void nextLab(void);
+
+/*
+                         Main application
+ */
+void main(void) {
+    SYSTEM_Initialize();
+    
+    // Disable the pre-enabled interrupts of the MCC    
+    INTERRUPT_TMR0InterruptDisable();
+
+    
+    while (1){
+        checkButtonS1();
+        switch(labNumber){
+            case 1:     HelloWorld();       break;
+            case 2:     Blink();            break;
+            case 3:     Rotate();           break;
+            case 4:     ADC();              break;
+            case 5:     VSR();              break;
+            case 6:     PWM();              break;
+            case 7:     Timer1();           break;
+            case 8:     Interrupt();        break;
+            case 9:     SleepWakeUp();      break;
+            case 10:    EEPROM();           break;
+        }
+        
+        if (switchEvent) {
+            nextLab();
+        }                                                   
+    }
+}
+
+void checkButtonS1(void){
+    if(btnState == NOT_PRESSED){
+        if(SWITCH_S1_PORT == LOW){  
+            __delay_ms(100);
+            btnState = PRESSED;
+        }
+    }else if(SWITCH_S1_PORT == HIGH){
+            btnState = NOT_PRESSED;   
+            switchEvent = 1;                                                   
+    }
+}
+
+void nextLab(void) {
+    switchEvent = 0; 
+    labNumber++;                                                        
+
+    if (labNumber > 10) {
+        labNumber = 1;
+    }
+}
+/**
+ End of File
+*/
